f194130c26ab2b652eb4946ffbc20509bb9a4f5a
dashprintingservices <dashprintingservices@gmail.com>
2020-02-10 23:40:24 +0800

    JS FILES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {HttpClient } from "@angular/common/http";
+import {User} from "./User";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'dash-angular';
+  user: User;
+  constructor(private http: HttpClient) {};
+  ngOnInit(): void {
+    this.http.get<User>('http://localhost:8000/auth').subscribe(data=>{
+      console.log(data);
+      this.user=data;
+    });
+  }
 }
