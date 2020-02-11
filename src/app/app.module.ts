11cdd43828878b4eab80d22e3c494ec238fe9922
dashprintingservices <dashprintingservices@gmail.com>
2020-02-11 18:54:30 +0800

    [Added the module and components]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {  ClientsModule} from "./clients/clients.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SalesModule } from './sales/sales.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ClientsModule,
+    SalesModule,
+    InventoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clients/clients.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientListComponent } from './client-list/client-list.component';
+
+
+
+@NgModule({
+  declarations: [ClientListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClientsModule { }
